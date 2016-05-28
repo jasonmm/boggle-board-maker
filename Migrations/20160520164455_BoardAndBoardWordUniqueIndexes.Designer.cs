2606ba549b26cf48ed3f2611af6fb6c9eacd0a52
Jason <jasonmm.github@gmail.com>
2016-05-27 20:02:15 -0500

    Added unique indexes to the database.

--- Migrations/20160520164455_BoardAndBoardWordUniqueIndexes.Designer.cs
+++ Migrations/20160520164455_BoardAndBoardWordUniqueIndexes.Designer.cs
@@ -8,9 +8,10 @@ using BoggleBoardMaker;
 namespace BoggleBoardMaker.Migrations
 {
     [DbContext(typeof(BoggleBoardContext))]
-    partial class BoggleBoardContextModelSnapshot : ModelSnapshot
+    [Migration("20160520164455_BoardAndBoardWordUniqueIndexes")]
+    partial class BoardAndBoardWordUniqueIndexes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.0-rc2-20896");
---
+++ Migrations/20160520164455_BoardAndBoardWordUniqueIndexes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BoggleBoardMaker.Migrations
+{
+    public partial class BoardAndBoardWordUniqueIndexes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_BoardWords_BoardId_Word",
+                table: "BoardWords",
+                columns: new[] { "BoardId", "Word" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Boards_BoardStr",
+                table: "Boards",
+                column: "BoardStr",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_BoardWords_BoardId_Word",
+                table: "BoardWords");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Boards_BoardStr",
+                table: "Boards");
+        }
+    }
+}
